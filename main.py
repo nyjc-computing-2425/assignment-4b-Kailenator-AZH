03d7be6fb46150822eabfbb04c7a6665fc50bf11
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-03-19 06:06:39 +0000

    Finished submission

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
-
-# Type your code below
+if all([i in "0123456789xE.^-" for i in stdform]) and stdform.count('.')<=1 and stdform.count('x')<=1 and stdform.count('^')<=1:
+  stdform = stdform.replace("x10^","E")
+  print("This number in E notation is "+stdform+".")
+else:
+  print("Error converting to scientific E notation.")
